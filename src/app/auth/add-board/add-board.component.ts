85b64e04b6547be754a25d0db70376cbbe1db930
Sourabhkostiodt <103024664+Sourabhkostiodt@users.noreply.github.com>
2022-07-04 17:29:32 +0530

    final

--- src/app/auth/add-board/add-board.component.ts
+++ src/app/auth/add-board/add-board.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChild,  ViewContainerRef} from '@angular/core';
+import { ResizableDraggableComponent } from '../resizable-draggable/resizable-draggable.component';
 
 @Component({
   selector: 'app-add-board',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-board.component.css']
 })
 export class AddBoardComponent implements OnInit {
+  @ViewChild('container1', { read: ViewContainerRef ,static: true }) container!: ViewContainerRef | any;
+  private _counter = 1;
 
-  constructor() { }
+  constructor(private viewContainerRef: ViewContainerRef) { }
 
   ngOnInit(): void {
+  }
+
+  number() : void{
+    this.container.clear();
+    const componentFactory = this.viewContainerRef.createComponent(ResizableDraggableComponent);
+    const componentRef = this.container.createComponent(componentFactory);
+    componentRef.instance.index = this._counter++;
+  }
+  pie(){
+    alert('pie')
+
+  }
+  bar(){
+    alert('bar')
+
+  }
+  line(){
+    alert('line')
+
   }
 
 }
